c90f5bf6636f35493b02aa9ec84ffa3aa09709b9
Harielson <harielson.mendes@gmail.com>
2021-07-10 11:08:03 -0300

    Criando sub classes

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Analista.kt
@@ -0,0 +1,11 @@
+package one.digitalinnovation.digionebank
+
+class Analista(nome: String,
+               cpf: String,
+               salario: Double) : Funcionario(
+    nome,
+    cpf,
+    salario
+) {
+    override fun calcularAuxilio() = salario + (salario * 0.1)
+}
--- src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
@@ -1,12 +1,17 @@
 package one.digitalinnovation.digionebank
 
-import java.math.BigDecimal
-
 abstract class Funcionario(
     nome: String,
     cpf: String,
-    val salario: BigDecimal
+    val salario: Double
 ) : Pessoa(nome, cpf){
 
-    abstract  fun calcularAuxilio():
+    protected abstract fun calcularAuxilio() : Double
+
+    override fun toString(): String = """
+    nome: $nome
+    cpf: $cpf
+    salario: $salario
+    auxilio: ${calcularAuxilio()}
+    """.trimIndent()
 }
